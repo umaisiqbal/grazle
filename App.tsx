07e2dd3899496cd4f6583dcdf0ac4a84a6c81802
umaisiqbal <82200623+umaisiqbal@users.noreply.github.com>
2024-04-01 14:01:41 -0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+import BuyGrazle from './src/BuyGrazle'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>hlo9</Text>
+      </View>
+    )
+  }
+}
---
+++ BuyGrazle.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class BuyGrazle extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={require('C:\Users\HP\Downloads')} />
+        <Text>BuyGrazle</Text>
+      </View>
+    )
+  }
+}
